6a77be2cb7921377e7fff537e1ad4b83fecce269
wignn <tigfiragnafatur1933@gmail.com>
2024-11-09 21:46:46 +0700

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div className="flex-center h-screen">
+      <h1 className="h1">
+        StoreIt -The only place to store your files
+      </h1>
+    </div>
+  );
+}
---
+++ src/components/AuthForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type FormType = 'sign-in' | 'sign-up'
+
+const AuthForm = ({type}:{type:FormType}) => {
+  return (
+    <div>AuthForm</div>
+  )
+}
+
+export default AuthForm
