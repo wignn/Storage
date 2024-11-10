0fecfd3c835057f290e85aa2e03a797b7f73b721
wignn <tigfiragnafatur1933@gmail.com>
2024-11-10 12:50:28 +0700

    refactor: update layout and authentication components; add new utility components

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,6 +1,5 @@
-import AuthForm from '@/components/AuthForm';
-import React from 'react'
+import AuthForm from "@/components/AuthForm";
 
-const SignIn = () => <AuthForm type="sign-in"/>;
+const SignIn = () => <AuthForm type="sign-in" />;
 
 export default SignIn;
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,6 +1,5 @@
-import AuthForm from '@/components/AuthForm'
-import React from 'react'
+import AuthForm from "@/components/AuthForm";
 
-const SignUp = () => <AuthForm type="sign-in"/>;
+const SignUp = () => <AuthForm type="sign-up" />;
 
-export default SignUp
+export default SignUp;
