958750526a335ae6328a923f13b7095c48597d85
wignn <tigfiragnafatur1933@gmail.com>
2025-07-10 12:54:19 +0700

    Implement code changes to enhance functionality and improve performance

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -38,7 +38,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <section className="flex flex-1 flex-col items-center bg-[#0f0715] p-4 py-10 lg:justify-center lg:p-10 lg:py-0">
         <div className="mb-16 lg:hidden">
           <Image
-            src="/assets/icons/logo-full-brand.svg"
+            src="/favicon.png"
             alt="logo"
             width={224}
             height={82}
